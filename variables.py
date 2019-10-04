f950a43a1fd5362ccc3bd71c9b1782fda0cd7a5a
AlexLUdo <55854439+AlexLUdo@users.noreply.github.com>
2019-10-04 21:26:05 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,17 @@
+"""TOY"""
+what_is ='Bear'
+print(type(what_is))
+alive = True
+print(type(alive))
+size = "XXXL"
+print(type(size))
+haitgh = 200.5
+print(type(haitgh))
+type_is = 'Angry'
+print(type(type_is))
+nice = True
+print(type(nice))
+colore = 'Brown'
+print(type(colore))
+soft = 5
+print(type(soft))
